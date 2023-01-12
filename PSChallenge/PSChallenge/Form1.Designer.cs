c96521ac83cf1be8e308063a8a0552544a816a94
jason-smith-plexus <120585864+jason-smith-plexus@users.noreply.github.com>
2023-01-12 09:16:02 -0600

    Added Program

---
+++ PSChallenge/PSChallenge/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace PSChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pButton1 = new System.Windows.Forms.Button();
+            this.con_1 = new System.Windows.Forms.Button();
+            this.Ch_Volt2 = new System.Windows.Forms.TextBox();
+            this.Ch_Volt1 = new System.Windows.Forms.TextBox();
+            this.Act_Volt1 = new System.Windows.Forms.TextBox();
+            this.Act_Volt2 = new System.Windows.Forms.TextBox();
+            this.Act_Current2 = new System.Windows.Forms.TextBox();
+            this.Ch_Current1 = new System.Windows.Forms.TextBox();
+            this.Ch_Current2 = new System.Windows.Forms.TextBox();
+            this.con_2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pButton2 = new System.Windows.Forms.Button();
+            this.Power_2 = new System.Windows.Forms.Label();
+            this.Power_1 = new System.Windows.Forms.Label();
+            this._Session = new System.Windows.Forms.Label();
+            this.Act_Current1 = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.openSession = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pButton1
+            // 
+            this.pButton1.BackColor = System.Drawing.Color.Red;
+            this.pButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pButton1.Location = new System.Drawing.Point(442, 89);
+            this.pButton1.Name = "pButton1";
+            this.pButton1.Size = new System.Drawing.Size(112, 90);
+            this.pButton1.TabIndex = 0;
+            this.pButton1.Text = "On/Off";
+            this.pButton1.UseVisualStyleBackColor = false;
+            this.pButton1.Click += new System.EventHandler(this.pButton1_Click);
+            // 
+            // con_1
+            // 
+            this.con_1.Location = new System.Drawing.Point(671, 267);
+            this.con_1.Name = "con_1";
+            this.con_1.Size = new System.Drawing.Size(86, 23);
+            this.con_1.TabIndex = 4;
+            this.con_1.Text = "Confirm";
+            this.con_1.UseVisualStyleBackColor = true;
+            this.con_1.Click += new System.EventHandler(this.con_1_Click);
+            // 
+            // Ch_Volt2
+            // 
+            this.Ch_Volt2.Location = new System.Drawing.Point(406, 305);
+            this.Ch_Volt2.Name = "Ch_Volt2";
+            this.Ch_Volt2.Size = new System.Drawing.Size(86, 20);
+            this.Ch_Volt2.TabIndex = 6;
+            // 
+            // Ch_Volt1
+            // 
+            this.Ch_Volt1.Location = new System.Drawing.Point(406, 267);
+            this.Ch_Volt1.Name = "Ch_Volt1";
+            this.Ch_Volt1.Size = new System.Drawing.Size(86, 20);
+            this.Ch_Volt1.TabIndex = 8;
+            // 
+            // Act_Volt1
+            // 
+            this.Act_Volt1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Act_Volt1.Location = new System.Drawing.Point(124, 267);
+            this.Act_Volt1.Name = "Act_Volt1";
+            this.Act_Volt1.ReadOnly = true;
+            this.Act_Volt1.Size = new System.Drawing.Size(86, 20);
+            this.Act_Volt1.TabIndex = 10;
+            // 
+            // Act_Volt2
+            // 
+            this.Act_Volt2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Act_Volt2.Location = new System.Drawing.Point(124, 305);
+            this.Act_Volt2.Name = "Act_Volt2";
+            this.Act_Volt2.ReadOnly = true;
+            this.Act_Volt2.Size = new System.Drawing.Size(86, 20);
+            this.Act_Volt2.TabIndex = 12;
+            // 
+            // Act_Current2
+            // 
+            this.Act_Current2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Act_Current2.Location = new System.Drawing.Point(265, 305);
+            this.Act_Current2.Name = "Act_Current2";
+            this.Act_Current2.ReadOnly = true;
+            this.Act_Current2.Size = new System.Drawing.Size(86, 20);
+            this.Act_Current2.TabIndex = 13;
+            // 
+            // Ch_Current1
+            // 
+            this.Ch_Current1.Location = new System.Drawing.Point(544, 267);
+            this.Ch_Current1.Name = "Ch_Current1";
+            this.Ch_Current1.Size = new System.Drawing.Size(86, 20);
+            this.Ch_Current1.TabIndex = 15;
+            // 
+            // Ch_Current2
+            // 
+            this.Ch_Current2.Location = new System.Drawing.Point(543, 305);
+            this.Ch_Current2.Name = "Ch_Current2";
+            this.Ch_Current2.Size = new System.Drawing.Size(86, 20);
+            this.Ch_Current2.TabIndex = 14;
+            // 
+            // con_2
+            // 
+            this.con_2.Location = new System.Drawing.Point(671, 305);
+            this.con_2.Name = "con_2";
+            this.con_2.Size = new System.Drawing.Size(86, 23);
+            this.con_2.TabIndex = 17;
+            this.con_2.Text = "Confirm";
+            this.con_2.UseVisualStyleBackColor = true;
+            this.con_2.Click += new System.EventHandler(this.con_2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Active Voltage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(262, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Active Current";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(403, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Change Voltage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(524, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 16);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Change Current Limit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Output 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Output 2";
+            // 
+            // pButton2
+            // 
+            this.pButton2.BackColor = System.Drawing.Color.Red;
+            this.pButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pButton2.Location = new System.Drawing.Point(579, 89);
+            this.pButton2.Name = "pButton2";
+            this.pButton2.Size = new System.Drawing.Size(112, 90);
+            this.pButton2.TabIndex = 24;
+            this.pButton2.Text = "On/Off";
+            this.pButton2.UseVisualStyleBackColor = false;
+            this.pButton2.Click += new System.EventHandler(this.pButton2_Click);
+            // 
+            // Power_2
+            // 
+            this.Power_2.AutoSize = true;
+            this.Power_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Power_2.Location = new System.Drawing.Point(576, 58);
+            this.Power_2.Name = "Power_2";
+            this.Power_2.Size = new System.Drawing.Size(96, 16);
+            this.Power_2.TabIndex = 25;
+            this.Power_2.Text = "Output 2 Power";
+            // 
+            // Power_1
+            // 
+            this.Power_1.AutoSize = true;
+            this.Power_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Power_1.Location = new System.Drawing.Point(439, 58);
+            this.Power_1.Name = "Power_1";
+            this.Power_1.Size = new System.Drawing.Size(96, 16);
+            this.Power_1.TabIndex = 26;
+            this.Power_1.Text = "Output 1 Power";
+            // 
+            // _Session
+            // 
+            this._Session.AutoSize = true;
+            this._Session.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._Session.Location = new System.Drawing.Point(196, 58);
+            this._Session.Name = "_Session";
+            this._Session.Size = new System.Drawing.Size(97, 16);
+            this._Session.TabIndex = 27;
+            this._Session.Text = "Select Session";
+            // 
+            // Act_Current1
+            // 
+            this.Act_Current1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Act_Current1.Location = new System.Drawing.Point(265, 267);
+            this.Act_Current1.Name = "Act_Current1";
+            this.Act_Current1.ReadOnly = true;
+            this.Act_Current1.Size = new System.Drawing.Size(86, 20);
+            this.Act_Current1.TabIndex = 7;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(199, 84);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(201, 95);
+            this.listBox.TabIndex = 28;
+            this.listBox.DoubleClick += new System.EventHandler(this.listBox_DoubleClick);
+            // 
+            // openSession
+            // 
+            this.openSession.Location = new System.Drawing.Point(42, 84);
+            this.openSession.Name = "openSession";
+            this.openSession.Size = new System.Drawing.Size(121, 35);
+            this.openSession.TabIndex = 29;
+            this.openSession.Text = "Open Session";
+            this.openSession.UseVisualStyleBackColor = true;
+            this.openSession.Click += new System.EventHandler(this.openSession_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 30;
+            this.textBox1.Text = "Selected Session";
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.HelpBalloon;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openSession);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this._Session);
+            this.Controls.Add(this.Power_1);
+            this.Controls.Add(this.Power_2);
+            this.Controls.Add(this.pButton2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.con_2);
+            this.Controls.Add(this.Ch_Current1);
+            this.Controls.Add(this.Ch_Current2);
+            this.Controls.Add(this.Act_Current2);
+            this.Controls.Add(this.Act_Volt2);
+            this.Controls.Add(this.Act_Volt1);
+            this.Controls.Add(this.Ch_Volt1);
+            this.Controls.Add(this.Act_Current1);
+            this.Controls.Add(this.Ch_Volt2);
+            this.Controls.Add(this.con_1);
+            this.Controls.Add(this.pButton1);
+            this.Name = "Form1";
+            this.Text = "CPX400DP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pButton1;
+        private System.Windows.Forms.Button con_1;
+        private System.Windows.Forms.TextBox Ch_Volt2;
+        private System.Windows.Forms.TextBox Ch_Volt1;
+        private System.Windows.Forms.TextBox Act_Volt1;
+        private System.Windows.Forms.TextBox Act_Volt2;
+        private System.Windows.Forms.TextBox Act_Current2;
+        private System.Windows.Forms.TextBox Ch_Current1;
+        private System.Windows.Forms.TextBox Ch_Current2;
+        private System.Windows.Forms.Button con_2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button pButton2;
+        private System.Windows.Forms.Label Power_2;
+        private System.Windows.Forms.Label Power_1;
+        private System.Windows.Forms.Label _Session;
+        private System.Windows.Forms.TextBox Act_Current1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button openSession;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PSChallenge/PSChallenge/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NationalInstruments.Visa;
+
+namespace PSChallenge
+{
+    public partial class Form1 : Form
+    {
+
+        private MessageBasedSession mbSession;
+        ResourceManager rmSession = new ResourceManager();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        // Finds the resources available to use and adds to listbox
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var rmSession = new ResourceManager())
+            {
+                var resources = rmSession.Find("(ASRL|GPIB|TCPIP|USB)?*");
+                foreach (string s in resources)
+                {
+                    listBox.Items.Add(s);
+                }
+            }
+        }
+
+        // Connects to the resource and displays Active Voltages and Current Limits and Powers off the Power Supply
+        private void openSession_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mbSession = (MessageBasedSession)rmSession.Open(textBox1.Text);
+                MessageBox.Show("Resource Connected");
+
+                mbSession.RawIO.Write("V1?\n");
+                Act_Volt1.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+
+                mbSession.RawIO.Write("V2?\n");
+                Act_Volt2.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+
+                mbSession.RawIO.Write("I1?\n");
+                Act_Current1.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+
+                mbSession.RawIO.Write("I2?\n");
+                Act_Current2.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+
+                mbSession.RawIO.Write("OP1 0\n");
+
+                mbSession.RawIO.Write("OP2 0\n");
+            }
+            catch (InvalidCastException)
+            {
+                MessageBox.Show("Resource must be a message-based session");
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+        }
+
+        // Selects specific resource
+        private void listBox_DoubleClick(object sender, EventArgs e)
+        {
+            string selected = (string)listBox.SelectedItem;
+            textBox1.Text = selected;
+        }
+
+        //Allow the button to turn on/off Output 1 and diplays the button as a color (Green = On, Red = Off)
+        private void pButton1_Click(object sender, EventArgs e)
+        {
+            if (pButton1.BackColor == System.Drawing.Color.Green) {
+                mbSession.RawIO.Write("OP1 0\n");
+                pButton1.BackColor = System.Drawing.Color.Red;
+            }
+            else {
+                mbSession.RawIO.Write("OP1 1\n");
+                pButton1.BackColor = System.Drawing.Color.Green;
+            }
+        }
+
+        //Allow the button to turn on/off Output 2 and diplays the button as a color (Green = On, Red = Off)
+        private void pButton2_Click(object sender, EventArgs e)
+        {
+            if (pButton2.BackColor == System.Drawing.Color.Green)
+            {
+                mbSession.RawIO.Write("OP2 0\n");
+                pButton2.BackColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                mbSession.RawIO.Write("OP2 1\n");
+                pButton2.BackColor = System.Drawing.Color.Green;
+            }
+        }
+
+        //Sets the Voltage and Current Limit of the first output and throws error if Voltage or Current Limits are breached
+        private void con_1_Click(object sender, EventArgs e)
+        {
+            if (Int32.Parse(Ch_Volt1.Text) > 60 || Int32.Parse(Ch_Volt1.Text) < 0)
+            {
+                MessageBox.Show("Voltage Not Within Limits of 0 - 60");
+                return;
+            }
+            if (Int32.Parse(Ch_Current1.Text) > 20 || Int32.Parse(Ch_Current1.Text) < 0)
+            {
+                MessageBox.Show("Current Not Within Limits of 0 - 20");
+                return;
+            }
+
+            try
+            {
+                mbSession.RawIO.Write("V1 " + Ch_Volt1.Text + "\n");
+                mbSession.RawIO.Write("V1?\n");
+                Act_Volt1.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+
+                mbSession.RawIO.Write("I1 " + Ch_Current1.Text + "\n");
+                mbSession.RawIO.Write("I1?\n");
+                Act_Current1.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+        }
+
+        //Sets the Voltage and Current Limit of the second output and throws error if Voltage or Current Limits are breached
+        private void con_2_Click(object sender, EventArgs e)
+        {
+            if (Int32.Parse(Ch_Volt2.Text) > 60 || Int32.Parse(Ch_Volt2.Text) < 0)
+            {
+                MessageBox.Show("Voltage Not Within Limits of 0 - 60");
+                return;
+            }
+            if (Int32.Parse(Ch_Current2.Text) > 20 || Int32.Parse(Ch_Current2.Text) < 0)
+            {
+                MessageBox.Show("Current Not Within Limits of 0 - 20");
+                return;
+            }
+            try
+            {
+                mbSession.RawIO.Write("V2 " + Ch_Volt2.Text + "\n");
+                mbSession.RawIO.Write("V2?\n");
+                Act_Volt2.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+
+                mbSession.RawIO.Write("I2 " + Ch_Current2.Text + "\n");
+                mbSession.RawIO.Write("I2?\n");
+                Act_Current2.Text = mbSession.RawIO.ReadString().Remove(0, 3);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+    }
+}
